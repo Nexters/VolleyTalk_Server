a9900b3a1699932181a1b3a245bcb853364d8e68
Mike Kim <nser789@gmail.com>
2017-07-29 00:18:57 +0900

    .travis.yml 수정

---
+++ models/tb_follows.js
@@ -0,0 +1,39 @@
+/* jshint indent: 2 */
+
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('tb_follows', {
+    seq: {
+      type: DataTypes.INTEGER(11),
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    followtype: {
+      type: DataTypes.STRING(10),
+      allowNull: false
+    },
+    typeseq: {
+      type: DataTypes.INTEGER(11),
+      allowNull: false
+    },
+    userid: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+      references: {
+        model: 'tb_userinfos',
+        key: 'userid'
+      }
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: sequelize.literal('CURRENT_TIMESTAMP')
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: true
+    }
+  }, {
+    tableName: 'tb_follows'
+  });
+};
